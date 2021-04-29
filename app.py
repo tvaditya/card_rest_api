4d4e3527aa6b780cd9d60e7ced6e8287effc7dae
tvaditya <tvaditya@gmail.com>
2021-04-28 21:15:56 -0300

    App bug corrected

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+from views.cards import card_blueprint
+
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(64)
+app.config.update(ADMIN=os.environ.get("ADMIN"))
+
+app.register_blueprint(card_blueprint, url_prefix="/cards")
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
